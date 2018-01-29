701b5e50067fb559c0f87dc19bd4ac24c59bc744
Mark <shuyumark@gmail.com>
2018-01-29 14:32:44 +0800

    udpate

--- comment-app/src/Comment.js
+++ comment-app/src/Comment.js
@@ -42,13 +42,24 @@ class Comment extends Component {
         }
     }
 
+    _getProcessedContent(content) {
+        return content.replace(/&/g, "&amp;")
+                    .replace(/</g, "&lt;")
+                    .replace(/>/g, "&gt;")
+                    .replace(/"/g, "&quot;")
+                    .replace(/'/g, "&#039;")
+                    .replace(/`([\S\s]+?)`/g, '<code>$1</code>')
+    }
+
     render() {
         return (
             <div className='comment'>
                 <div className='comment-user'>
                     <span>{this.props.comment.username}</span>:
                 </div>
-                <p>{this.props.comment.content}</p>
+                <p dangerouslySetInnerHTML={{
+                    __html: this._getProcessedContent(this.props.comment.content)
+                }}></p>
                 <span className='comment-createdtime'>{this.state.timeString}</span>
                 <span onClick={this.handleDelete.bind(this)} className='comment-delete'>Delete</span>
             </div>
