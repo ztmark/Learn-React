6c320bc05c8063758372826bfecae7d672cae54a
Mark <shuyumark@gmail.com>
2018-02-01 11:47:59 +0800

    update

--- make-react-redux/src/index.js
+++ make-react-redux/src/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
 import Header from './Header'
 import Content from './Content'
-import { Provider } from './react-redux'
+import { Provider } from 'react-redux'
 import './index.css'
 
 
@@ -17,19 +17,6 @@ class Index extends Component {
             </div>
         )
     }
-}
-
-function createStore(reducer) {
-    let state = null
-    const listeners = []
-    const subscribe = (listener) => listeners.push(listener)
-    const getState = () => state
-    const dispatch = (action) => {
-        state = reducer(state, action)
-        listeners.forEach((listener) => listener())
-    }
-    dispatch({}) // 初始化 state
-    return { getState, dispatch, subscribe }
 }
 
 const themeReducer = (state, action) => {
